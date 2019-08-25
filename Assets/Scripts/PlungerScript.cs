b9d1f486104e26385d1786399df255f9bb8aa2f9
Ben <benjamin.musil@gmail.com>
2019-08-25 18:23:13 -0500

    Initial commit

---
+++ Assets/Scripts/PlungerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class PlungerScript : MonoBehaviour {
+	void Start() {
+		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+		CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+		int i = 0;
+		while (i < meshFilters.Length) {
+			combine[i].mesh = meshFilters[i].sharedMesh;
+			combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+			meshFilters[i].gameObject.SetActive(false);
+			i++;
+		}
+		transform.GetComponent<MeshFilter>().mesh = new Mesh();
+		transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+		transform.gameObject.SetActive(true);
+		var mc = gameObject.AddComponent (typeof(MeshCollider)) as MeshCollider;
+
+		mc.convex = true;
+
+		transform.position = new Vector3 (0, 0, 0);
+
+	}
+}
---
+++ Assets/Scripts/Push.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody cap;
+
+	// Use this for initialization
+	void Start () {
+		cap = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (0.0f, moveVertical, 0.0f);
+
+		cap.AddForce (movement * speed);
+	}
+
+}
